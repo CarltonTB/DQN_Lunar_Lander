94325848e79a52b6f08cc56e0234575c73a7363b
Carlton Brady <ctombrady@gmail.com>
2020-10-26 23:56:55 -0400

    agent records experience now

--- dqn_agent.py
+++ dqn_agent.py
@@ -1,13 +1,15 @@
 # Author: Carlton Brady
 
 import random
+from collections import deque
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 class DQNLunarLanderAgent:
-
-    def __init__(self, epsilon):
-        self.experience_memory = []
-        self.q_network = None
+    def __init__(self, epsilon, q_network, max_memory_length):
+        self.experience_memory = deque(maxlen=max_memory_length)
+        self.q_network = q_network
         self.epsilon = epsilon
 
     def select_action(self):
@@ -20,6 +22,7 @@ class DQNLunarLanderAgent:
         if random.random() <= self.epsilon:
             return random.randint(0, 4)
         else:
+            # Feed forward the q network and take the action with highest q value
             return 0
 
     def sample_random_experience(self, n):
@@ -30,11 +33,30 @@ class DQNLunarLanderAgent:
         """
         return random.sample(self.experience_memory, n)
 
+    def push_memory(self, memory):
+        """Push a transition memory object onto the experience deque"""
+        assert (isinstance(memory, TransitionMemory))
+        self.experience_memory.append(memory)
+
 
 class TransitionMemory:
-
-    def __init__(self, state, action, reward, next_state):
+    def __init__(self, state, action, reward, next_state, done):
         self.state = state
         self.action = action
         self.reward = reward
         self.next_state = next_state
+        self.done = done
+
+
+class DQN(nn.Module):
+    def __init__(self):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(8, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 4)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
--- environment.py
+++ environment.py
@@ -2,31 +2,39 @@
 
 import gym
 import time
+from dqn_agent import *
+import torch
 
 
-def run_lunar_lander():
+def run_lunar_lander(agent):
     """Run the lunar lander v2 environment"""
     start = time.time()
     # Make lunar lander v2 environment
     env = gym.make('LunarLander-v2')
     env.reset()
-    print(env.action_space)
-    print(env.observation_space)
+    # print(env.action_space)
+    # print(env.observation_space)
     for episode in range(100):
+        state = None
         for t in range(1000):
+            if t > 0:
+                state = observation
             # env.render()
             # take a random action
-            randAction = env.action_space.sample()
-            observation, reward, done, info = env.step(randAction)
+            action = env.action_space.sample()
+            observation, reward, done, info = env.step(action)
+            next_state = observation
+            # TODO: what should the state be if it's the first timestep? all zeros?
+            agent.push_memory(TransitionMemory(state, action, reward, next_state, done))
             if done:
                 # print(f'Episode {episode} ended in {t + 1} timesteps')
                 break
-
         env.reset()
-
     env.close()
     print(f'Time elapsed: {time.time() - start} seconds')
+    print(len(agent.experience_memory))
 
 
 if __name__ == "__main__":
-    run_lunar_lander()
+    agent = DQNLunarLanderAgent(epsilon=0.1, q_network=DQN(), max_memory_length=10000)
+    run_lunar_lander(agent)
